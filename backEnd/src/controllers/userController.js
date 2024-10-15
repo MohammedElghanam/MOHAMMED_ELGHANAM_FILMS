ad89021fe55e3f4ea4421c79810cd617b9f49855
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-10-15 13:52:31 +0100

    add method login

--- backEnd/src/controllers/userController.js
+++ backEnd/src/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
 class UserController{
 
     static async register(req, res) {
@@ -17,6 +19,31 @@ class UserController{
             return res.status(400).json({ message: error.message });
         }
           
+    }
+
+    static async login(req, res) {
+
+        const { email, password } = req.body;
+
+        try {
+
+            const user = await User.findOne({ email });
+
+            if (!user) return res.status(400).json({ message: 'User not found' });
+            
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+            
+            const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+                expiresIn: '1h',
+            });
+
+            res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });
+            return res.json({ user, token });
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
 }
--- backEnd/src/routes/userRoute.js
+++ backEnd/src/routes/userRoute.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 
 router.post('/register', userController.register);
+router.post('/login', userController.login);
+// router.post('/logout', userController.logout);
+// router.post('/forgotPassword', userController.forgotPassword);
+// router.post('/resetPassword', userController.resetPassword);
 
 module.exports = router;
